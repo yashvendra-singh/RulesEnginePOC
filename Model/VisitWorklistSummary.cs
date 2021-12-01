5321b906152a8b063a2ac3c998bf09faf855a7c7
yashvendra-singh <yashvendra.singh29@gmail.com>
2021-12-01 09:51:52 +0530

    first commit

---
+++ Model/VisitWorklistSummary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RulesEnginePOC.Model
+{
+    internal class VisitWorklistSummary
+    {
+        public int VisitKey { get; set; }
+        public int FacilityKey { get; set; }
+        public int PatientKey { get; set; }
+        public string PatientName { get; set; }
+        public DateTime AdmitDate { get; set; }
+        public DateTime? DischargeDate { get; set; }
+        public string ChiefComplaint { get; set; }
+        public int CDIPriorityKey { get; set; }
+        public PriorityFactor PriorityFactor { get; set; }
+    }
+
+    internal class PriorityFactor
+    {
+        public int CDIPriorityFactorKey { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using RulesEnginePOC.Model;
+using RulesEngine.Models;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Dynamic;
+using Newtonsoft.Json.Converters;
+using RulesEngine.Extensions;
+
+namespace RulesEnginePOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var visit1 = new VisitWorklistSummary
+            {
+                VisitKey = 25,
+                AdmitDate = DateTime.Now.AddDays(-3),
+                FacilityKey = 1,
+                CDIPriorityKey =  -30,
+                ChiefComplaint = "Chest Pressure",
+                DischargeDate = null,
+                PatientKey = 3342,
+                PatientName = "Adam Roberts",
+                PriorityFactor = new PriorityFactor { CDIPriorityFactorKey = 11}
+            };
+
+            var inputs = new dynamic[]
+                {
+                    visit1
+                };
+
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "Rules.json", SearchOption.AllDirectories);
+            if (files == null || files.Length == 0)
+                throw new Exception("Rules not found.");
+
+            var fileData = File.ReadAllText(files[0]);
+            var workflow = JsonConvert.DeserializeObject<List<Workflow>>(fileData);
+
+            var bre = new RulesEngine.RulesEngine(workflow.ToArray(), null);
+
+            List<RuleResultTree> resultList = bre.ExecuteAllRulesAsync("ASDRG", inputs).Result;
+
+            resultList.OnSuccess((eventName) => {
+                Console.WriteLine($"ASDRG priorty factor is applicable with factor value {eventName}.");
+            });
+
+            resultList.OnFail(() => {
+                Console.WriteLine(resultList[0].ExceptionMessage);
+            });
+        }
+    }
+}
